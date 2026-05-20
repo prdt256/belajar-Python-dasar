d4f85ea59f673ef7a524f7d01376c4bec4c06854
prdt256 <mrxdoger@gmail.com>
2026-05-20 19:06:15 +0700

    episode 7 belum selesai

---
+++ 07 Mengambil Input Data dari User/main.py
@@ -0,0 +1,6 @@
+data_string = input("Masukkan Tipe Data String")
+
+data_integer = input(int("Masukkan Tipe Data Integer"))
+
+data_float =input(int("Masukkan Tipe Data Float"))
+data_float = float(data_float)
